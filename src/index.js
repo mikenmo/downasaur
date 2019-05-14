ed0f83f987dac4abe9ff5d692e185cd11efe3348
mikenmo <mdmojar@up.edu.ph>
2019-05-15 00:46:29 +0800

    Added pterodactyl with flapping animation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { mat4 } from "gl-matrix";
 import * as THREE from "three";
 
 import { initGraphics, Colors } from "./utils";
-import { Bird, Cactus } from "./objects";
+import { Bird, Cactus, Pterodactyl } from "./objects";
 import OrbitControls from "three-orbitcontrols";
 
 import renderHTML from './html';
@@ -24,8 +24,9 @@ function main(mount) {
   mount.appendChild(renderer.domElement);
 
   const cactus = new Cactus();
-  cactus.render(scene);
-
+  const ptero = new Pterodactyl();
+  // cactus.render(scene);
+  
   // const mesh = new THREE.Object3D();
 
   var geomCockpit = new THREE.BoxGeometry(60, 50, 50, 1, 1, 1);
@@ -37,8 +38,7 @@ function main(mount) {
   // cockpit.position.y = 100;
   scene.add(cockpit);
   
-  let wingPos = 0;
-  let wingIter = 15;
+  
 
   
   // var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -53,103 +53,15 @@ function main(mount) {
     while(scene.children.length > 0){ 
       scene.remove(scene.children[0]); 
     }
-    let vertices = [
-      //FRONT
-      //head
-      -0.3, 0.5, -0.1,  
-      -0.9, 0.0, -0.1,  
-      -0.4, 0.0, -0.1,  
-      //neck
-      -0.35, 0.25, -0.1,  
-      -0.4, 0.0, -0.1,    
-      -0.05, -0.1, 0.0,   
-      0.1, -0.5, -0.1,     
-      //body
-      -0.05, -0.1, 0.0,  
-      0.1, -0.5, -0.1,  
-      0.9, -0.1, 0.0,  
-      0.7, -0.5, -0.1,  
-  
-      //BACK
-      //head
-      -0.3, 0.5, -0.3,  
-      -0.9, 0.0, -0.3,  
-      -0.4, 0.0, -0.3,  
-      //neck
-      -0.35, 0.25, -0.3,  
-      -0.4, 0.0, -0.3,  
-      -0.05, -0.1, -0.4,  
-      0.1, -0.5, -0.3,  
-      //body
-      -0.05, -0.1, -0.4,  
-      0.1, -0.5, -0.3,  
-      0.9, -0.1, -0.4,  
-      0.7, -0.5, -0.3,  
-  
-      //wing
-      0.0, -0.1, -0.4,  
-      0.5, -0.1, -0.4,  
-      -0.1, 0.7-(wingPos*0.01),-1.0-(wingPos*0.01),   
-  
-      //wing
-      0.0, -0.1, 0.0,  
-      0.5, -0.1, 0.0,  
-      -0.1, 0.7-(wingPos*0.01),0.6+(wingPos*0.01),   
-  
-  
-    ];
-  
-    let indices = [
-      // indices of vertices forming triangles
-        //front
-        0,1,2,
-        3,4,5,  4,5,6,
-        7,8,9,  8,9,10,
-        //back
-        11,12,13,
-        14,15,16,  15,16,17,
-        18,19,20,  19,20,21,
-        //sides reuse other points
-        //head
-        0,11,2,    11,2,12,
-        1,12,2,    12,2,13,
-        0,11,3,    11,3,14,
-        //neck
-        3,14,5,    14,5,16,
-        4,15,6,    15,6,17,
-        //body
-        7,18,9,   18,9,20,
-        8,19,10,  19,10,21,
-        9,20,10,  20,10,21,
-        //wings
-        22,23,24,
-        25,26,27
-      ];
-    var pteroGeometry = new THREE.Geometry();
-    for(let i=0;i<vertices.length;i+=3){
-      pteroGeometry.vertices.push(new THREE.Vector3(vertices[i],vertices[i+1],vertices[i+2]));
-    }
-    for(let i=0;i<indices.length;i+=3){
-      pteroGeometry.faces.push( new THREE.Face3(indices[i],indices[i+1],indices[i+2]) );
-    }
-    material = new THREE.MeshBasicMaterial({
-      color: 0x00ff00,
-      side: THREE.DoubleSide,
-    });
+    // cactus.render(scene);
+    ptero.render(scene);
     
-    let ptero = new THREE.Mesh(pteroGeometry,material);
-    scene.add(ptero);
-    
+    console.log(scene.children);
     controls.update();
     renderer.clear();
     renderer.render(scene, camera);
-
-    wingPos+=wingIter;
-    if(wingPos<=0){
-      wingIter=15;
-    }else if(wingPos>=100){
-      wingIter=-15;
-    }
+    
+    
 
     requestAnimationFrame(animate);
   }
