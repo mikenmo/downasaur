cc862b8241c029acdcaddaf7e2a1c9d68a8ca015
mikenmo <mdmojar@up.edu.ph>
2019-05-15 01:04:24 +0800

    Edited index file

--- src/index.js
+++ src/index.js
@@ -5,90 +5,23 @@ import { initGraphics, Colors } from "./utils";
 import { Bird, Cactus, Pterodactyl } from "./objects";
 import OrbitControls from "three-orbitcontrols";
 
-import renderHTML from "./html";
-import "./stylesheets/fonts.css";
-import "./stylesheets/index.css";
-
-const CONSTANTS = {
-  PLANE_WIDTH: 50,
-  PLANE_LENGTH: 1000,
-  PADDING: 20
-};
-
-const GAME = {
-  renderer: null,
-  scene: null,
-  axesHelper: null,
-  camera: null,
-  player: null,
-  controls: null,
-  floor: null,
-  containerWidth: 0,
-  containerHeight: 0,
-  obstacles: [],
-  light: {
-    directional: null,
-    hemishphere: null
-  }
-};
-
-function Hero() {
-  var hero = {},
-    heroGeometry = {},
-    heroMaterial = {};
-
-  heroGeometry = new THREE.CylinderGeometry(0, 2, 5, 10);
-  heroMaterial = new THREE.MeshLambertMaterial({
-    color: 0xe91e63,
-    shading: THREE.FlatShading
-  });
-  hero = new THREE.Mesh(heroGeometry, heroMaterial);
-  hero.castShadow = true;
-  hero.position.set(0, 5, CONSTANTS.PLANE_LENGTH / 2);
-  hero.rotation.x = 0.785;
-
-  window.addEventListener("keydown", function() {
-    if (
-      event.keyCode === 37 &&
-      hero.position.x !== -(CONSTANTS.PLANE_WIDTH - CONSTANTS.PADDING) / 2
-    ) {
-      hero.position.x -= (CONSTANTS.PLANE_WIDTH - CONSTANTS.PADDING) / 2;
-    } else if (
-      event.keyCode === 39 &&
-      hero.position.x !== (CONSTANTS.PLANE_WIDTH - CONSTANTS.PADDING) / 2
-    ) {
-      hero.position.x += (CONSTANTS.PLANE_WIDTH - CONSTANTS.PADDING) / 2;
-    }
-  });
-
-  return hero;
-}
+import renderHTML from './html';
+import './stylesheets/fonts.css';
+import './stylesheets/index.css';
 
 function main(mount) {
-  const container = document.getElementById("root");
-  GAME.containerWidth = window.innerWidth;
-  GAME.containerHeight = window.innerHeight;
-  GAME.renderer = initGraphics().renderer;
-  GAME.renderer.setSize(GAME.containerWidth, GAME.containerHeight);
-  GAME.renderer.setClearColor(0xfffafa, 1);
-  GAME.renderer.shadowMap.enabled = true;
-  mount.appendChild(GAME.renderer.domElement);
-
-  GAME.scene = new THREE.Scene();
-
-  GAME.axesHelper = new THREE.AxesHelper(CONSTANTS.PLANE_LENGTH / 2);
-  // CAMERA
-  GAME.camera = new THREE.PerspectiveCamera(
-    45,
-    GAME.containerWidth / GAME.containerHeight,
-    1,
-    3000
+  const { renderer } = initGraphics();
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100
   );
-  GAME.camera.position.set(
-    0,
-    CONSTANTS.PLANE_LENGTH / 125,
-    CONSTANTS.PLANE_LENGTH / 2 + CONSTANTS.PLANE_LENGTH / 25
-  );
+  camera.position.set(0, 0, 100);
+  camera.position.z = 15;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  mount.appendChild(renderer.domElement);
 
   const cactus = new Cactus();
   const ptero = new Pterodactyl();
@@ -135,26 +68,4 @@ function main(mount) {
   animate();
 }
 
-function createCactus() {
-  const cactus = new Cactus();
-
-  const xPositionValues = [
-    -(CONSTANTS.PLANE_WIDTH - CONSTANTS.PADDING) / 2,
-    0,
-    (CONSTANTS.PLANE_WIDTH - CONSTANTS.PADDING) / 2
-  ];
-  const zPositionValues = [-(CONSTANTS.PLANE_LENGTH - CONSTANTS.PADDING) / 2];
-  console.log(xPositionValues);
-  const xPosition =
-    xPositionValues[getRandomInteger(0, xPositionValues.length - 1)];
-  const yPosition = 3;
-  const zPosition =
-    zPositionValues[getRandomInteger(0, zPositionValues.length - 1)];
-  cactus.render(GAME.scene, xPosition, yPosition, zPosition);
-  GAME.obstacles.push(cactus);
-}
-
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 renderHTML(main);
